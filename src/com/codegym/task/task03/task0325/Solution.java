8a2078e63f70aec55171a85aaa76478bd00e2c1b
Nate <github email address>
2019-06-06 14:13:30 -0400

    level 3, lesson 9

---
+++ src/com/codegym/task/task03/task0325/Solution.java
@@ -0,0 +1,21 @@
+package com.codegym.task.task03.task0325;
+
+import java.io.*;
+import java.util.Scanner;
+
+/* 
+Financial expectations
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        String phrase = "I will earn $" + n + " per hour";
+
+        System.out.println(phrase);
+    }
+}
